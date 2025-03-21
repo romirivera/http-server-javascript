db7f5b6a191d65511e07db7517fc6189a6df9068
Romina Rivera <rdrivera@uc.cl>
2025-03-20 23:54:43 -0300

    http response

--- app/main.js
+++ app/main.js
@@ -1,8 +1,14 @@
 const net = require('net');
 
 const server = net.createServer((socket) => {
+  socket.on('data', () => {
+    socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    socket.end();
+  });
   socket.on('close', () => {
     socket.end();
   });
 });
-server.listen(4221, 'localhost');
+server.listen(4221, 'localhost', () => {
+  console.log('Servidor escuchando en http://localhost:4221');
+});
