7ac10a58d56c3a7a4b8100fe1faf5aa3326607ac
Romina Rivera <rdrivera@uc.cl>
2025-04-07 23:58:09 -0400

    agregar manejo del header User-Agent y respuesta en la ruta /user-agent

--- app/main.js
+++ app/main.js
@@ -3,8 +3,14 @@ const net = require('net');
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
     const request = data.toString();
-    const requestLine = request.split('\r\n')[0]; // Primera línea
-    const [method, path, version] = requestLine.split(' '); // Divide en partes
+    const [requestLine, ...headerLines] = request.split('\r\n');
+    const [method, path] = requestLine.split(' ');
+
+    // Buscar el header User-Agent (es case-insensitive)
+    const userAgentLine = headerLines.find((line) =>
+      line.toLowerCase().startsWith('user-agent:')
+    );
+    const userAgent = userAgentLine ? userAgentLine.split(': ')[1] : '';
 
     // Lógica para responder según el path
     if (method === 'GET' && path === '/') {
@@ -16,10 +22,16 @@ const server = net.createServer((socket) => {
       const response =
         `HTTP/1.1 200 OK\r\n` +
         `Content-Type: text/plain\r\n` +
-        `Content-Length: ${contentLength}\r\n` +
-        `\r\n` +
+        `Content-Length: ${length}\r\n\r\n` +
         `${str}`;
-
+      socket.write(response);
+    } else if (method === 'GET' && path === '/user-agent') {
+      const length = Buffer.byteLength(userAgent);
+      const response =
+        `HTTP/1.1 200 OK\r\n` +
+        `Content-Type: text/plain\r\n` +
+        `Content-Length: ${length}\r\n\r\n` +
+        `${userAgent}`;
       socket.write(response);
     } else {
       socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
